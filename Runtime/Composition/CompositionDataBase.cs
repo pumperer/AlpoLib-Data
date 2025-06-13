a12dab7982e7b3211c79f81d5597d52c41f2e231
ALPO <pumperer@gmail.com>
2025-06-13 15:30:53 +0900

    Update CompositionDataBase.cs

--- Runtime/Composition/CompositionDataBase.cs
+++ Runtime/Composition/CompositionDataBase.cs
@@ -1,12 +1,14 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace alpoLib.Data.Composition
 {
-    public abstract record CompositionDataBase
+    public abstract class CompositionDataBase : DataManagerHolder
     {
     }
 
-    public abstract record CompositionDataBase<TBase, TUser> : CompositionDataBase
+    public abstract class CompositionDataBase<TBase, TUser> : CompositionDataBase, IEquatable<CompositionDataBase<TBase, TUser>>
         where TBase : TableDataBase
         where TUser : UserDataBase
     {
@@ -17,6 +19,26 @@ namespace alpoLib.Data.Composition
         {
             BaseData = baseData;
             UserData = userData;
+        }
+
+        public bool Equals(CompositionDataBase<TBase, TUser> other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return EqualityComparer<TBase>.Default.Equals(BaseData, other.BaseData) && EqualityComparer<TUser>.Default.Equals(UserData, other.UserData);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((CompositionDataBase<TBase, TUser>)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(BaseData, UserData);
         }
     }
 }
